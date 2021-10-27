743479aba35b2d7d8dd88b7643ac06373f5e2a24
Kendall Jacobs <kendalljjacobs@icloud.com>
2021-10-27 13:28:23 -0500

    Add Create Endpoints to allow creation of new supers

--- supers_project/supers/urls.py
+++ supers_project/supers/urls.py
@@ -1,10 +1,13 @@
 from django.urls.resolvers import URLPattern
 from . import views
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import path
 
 app_name = 'supers'
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('<int:super_id>/', views.detail, name = 'detail'),
+    path('new/', views.create, name = 'create')
+
 ]
--- supers_project/supers/views.py
+++ supers_project/supers/views.py
@@ -1,7 +1,11 @@
+# from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 from .models import Supers
+from django.urls import reverse
+
+
 all_supers = Supers.objects.all()
 
 
@@ -10,3 +14,27 @@ def index(request):
         'all_supers' : all_supers
     }
     return render(request, 'supers/index.html', context)
+
+def detail(request, super_id):
+        single_super = Supers.objects.get(pk=super_id)
+        context = {
+            'single_super' : single_super
+        }
+        return render(request, 'supers/detail.html', context)
+
+
+def create(request):
+        if request.method == "POST":
+            # Save the form contents as a new db object
+            # return to index
+            name = request.POST.get('name')
+            alter_ego = request.POST.get('alter_ego')
+            primary = request.POST.get('primary')
+            secondary = request.POST.get('secondary')
+            catchphrase = request.POST.get('catchphrase')
+            new_super = Supers(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+            new_super.save()
+            return HttpResponseRedirect(reverse('supers:index'))
+        else:
+            return render(request, 'supers/create.html')
+        
